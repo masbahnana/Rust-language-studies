bda59c3825957b1ce99b7529b317099c4915aeb5
Natalia Raythz <nraythz@gmail.com>
2018-11-22 01:49:04 -0200

    Update arithmetic.rs

--- arithmetic.rs
+++ arithmetic.rs
@@ -1,8 +1,3 @@
-// Task : To explain assignment operations in rust
-// Author : Vigneshwer
-// Version : 1.0
-// Date : 3 Dec 2016
-
 fn main(){
 	// Arithmetic Operations
 	println!("5 + 4 = {}", 5+4 );
